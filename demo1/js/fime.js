3710e701e2757c0e61123cc2f1bceb49b9e7d045
JRE-Sun <jresun@163.com>
2017-02-07 10:06:49 +0800

    加入触屏
    功能齐全了，不过导航里面的汽车栏目点击就消失。。不晓得为啥，

--- demo1/js/fime.js
+++ demo1/js/fime.js
@@ -65,10 +65,8 @@ window.onload = function() {
 
     // 固定导航条代码
     var navHeight = $(".left-menu").offset().top;
-    var navHeight2 = $(".mob-nav-menu").offset().top;
     // alert(navHeight);
     var navFix = $(".left-menu");
-    var navFix2 = $(".mob-nav-menu");
     $(window).scroll(function() {
             if ($(this).scrollTop() > navHeight) {
                 navFix.addClass("navFix");
@@ -79,64 +77,14 @@ window.onload = function() {
                 // $(".fixbg").css("display", "none");
                 // $(".content .div1 .header .navbar li a").removeClass("change");
             }
-
-            if ($(this).scrollTop() > navHeight2) {
-                navFix2.addClass("mob-nav-menu-top");
-                // $(".fixbg").css("display", "block");
-                // $(".content .div1 .header .navbar li a").addClass("change");
-            } else {
-                navFix2.removeClass("mob-nav-menu-top");
-                // $(".fixbg").css("display", "none");
-                // $(".content .div1 .header .navbar li a").removeClass("change");
-            }
         })
         // 固定导航条代码结束
-
-    var obj = document.querySelector('.mob-nav-menu');
-    var startX;
-    var startY;
-    $(".mob-nav-menu").on("touchstart", function(e) {
-        e.preventDefault();
-        var touch = event.targetTouches[0];
-        startX = touch.pageX,
-            startY = touch.pageY;
+    var swiper = new Swiper('.swiper-container', {
+        pagination: '.swiper-pagination',
+        slidesPerView: 6,
+        paginationClickable: true,
+        spaceBetween: 5,
+        freeMode: false,
+        initialSlide: 4
     });
-
-
-    obj.addEventListener('touchmove', function(event) {
-        // 如果这个元素的位置内只有一个手指的话
-        if (event.targetTouches.length == 1) {　　　　
-            event.preventDefault();
-            var touch = event.targetTouches[0];
-            moveEndX = touch.pageX,
-                moveEndY = touch.pageY,
-                X = moveEndX - startX,
-                Y = moveEndY - startY;
-
-            if (Math.abs(X) > Math.abs(Y) && X > 0) {
-                var a = $('.mob-nav-menu').offset().left;
-                if (a + 4 > 0) {
-                    obj.style.left = 0 + 'px';
-                } else {
-                    obj.style.left = a + 4 + 'px';
-                }
-
-            } else if (Math.abs(X) > Math.abs(Y) && X < 0) {
-                var a = $('.mob-nav-menu').offset().left;
-                if (a - 4 < -392) {
-                    obj.style.left = -392 + 'px';
-                } else {
-                    obj.style.left = a - 4 + 'px';
-                }
-            } else if (Math.abs(Y) > Math.abs(X) && Y > 0) {
-                // alert("top 2 bottom");
-            } else if (Math.abs(Y) > Math.abs(X) && Y < 0) {
-                // alert("bottom 2 top");
-            } else {
-                // alert("just touch");
-            }
-            // var a = $('.mob-nav-menu').offset().left;
-            // obj.style.left = a - 2 + 'px';
-        }
-    }, false);
 }
--- demo1/js/technology.js
+++ demo1/js/technology.js
@@ -1,6 +1,6 @@
 window.onload = function() {
     var app;
-    var newsId = "5572a10ab3cdc86cf39001f4";
+    var newsId = "5572a109b3cdc86cf39001e5";
     var firsturl = "https://route.showapi.com/109-35?channelId=" + newsId + "&maxResult=20&needAllList=0&needContent=0&needHtml=0&page=1&showapi_appid=31610&showapi_sign=794da37ef6d548bdb3faf07de393bc6d";
     $.ajax({
         type: "get",
@@ -65,10 +65,8 @@ window.onload = function() {
 
     // 固定导航条代码
     var navHeight = $(".left-menu").offset().top;
-    var navHeight2 = $(".mob-nav-menu").offset().top;
     // alert(navHeight);
     var navFix = $(".left-menu");
-    var navFix2 = $(".mob-nav-menu");
     $(window).scroll(function() {
             if ($(this).scrollTop() > navHeight) {
                 navFix.addClass("navFix");
@@ -79,64 +77,13 @@ window.onload = function() {
                 // $(".fixbg").css("display", "none");
                 // $(".content .div1 .header .navbar li a").removeClass("change");
             }
-
-            if ($(this).scrollTop() > navHeight2) {
-                navFix2.addClass("mob-nav-menu-top");
-                // $(".fixbg").css("display", "block");
-                // $(".content .div1 .header .navbar li a").addClass("change");
-            } else {
-                navFix2.removeClass("mob-nav-menu-top");
-                // $(".fixbg").css("display", "none");
-                // $(".content .div1 .header .navbar li a").removeClass("change");
-            }
         })
         // 固定导航条代码结束
-
-    var obj = document.querySelector('.mob-nav-menu');
-    var startX;
-    var startY;
-    $(".mob-nav-menu").on("touchstart", function(e) {
-        e.preventDefault();
-        var touch = event.targetTouches[0];
-        startX = touch.pageX,
-            startY = touch.pageY;
+    var swiper = new Swiper('.swiper-container', {
+        pagination: '.swiper-pagination',
+        slidesPerView: 6,
+        paginationClickable: true,
+        spaceBetween: 5,
+        freeMode: false
     });
-
-
-    obj.addEventListener('touchmove', function(event) {
-        // 如果这个元素的位置内只有一个手指的话
-        if (event.targetTouches.length == 1) {　　　　
-            event.preventDefault();
-            var touch = event.targetTouches[0];
-            moveEndX = touch.pageX,
-                moveEndY = touch.pageY,
-                X = moveEndX - startX,
-                Y = moveEndY - startY;
-
-            if (Math.abs(X) > Math.abs(Y) && X > 0) {
-                var a = $('.mob-nav-menu').offset().left;
-                if (a + 4 > 0) {
-                    obj.style.left = 0 + 'px';
-                } else {
-                    obj.style.left = a + 4 + 'px';
-                }
-
-            } else if (Math.abs(X) > Math.abs(Y) && X < 0) {
-                var a = $('.mob-nav-menu').offset().left;
-                if (a - 4 < -392) {
-                    obj.style.left = -392 + 'px';
-                } else {
-                    obj.style.left = a - 4 + 'px';
-                }
-            } else if (Math.abs(Y) > Math.abs(X) && Y > 0) {
-                // alert("top 2 bottom");
-            } else if (Math.abs(Y) > Math.abs(X) && Y < 0) {
-                // alert("bottom 2 top");
-            } else {
-                // alert("just touch");
-            }
-            // var a = $('.mob-nav-menu').offset().left;
-            // obj.style.left = a - 2 + 'px';
-        }
-    }, false);
 }
